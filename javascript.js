1ea10bcdc29492e14304630faf69fc86c40c98ca
AnnMilkei <86960339+AnnMilkei@users.noreply.github.com>
2022-02-13 15:32:04 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,49 @@
+let toggleNavStatus = false; 
+
+let toggleNav = function() {
+    let getSidebar = document.querySelector(".nav-sidebar");
+    let getSidebarUl = document.querySelector(".nav-sidebar ul");
+    let getSidebarTitle = document.querySelector(".nav-sidebar span");
+    let getSidebarLinks = document.querySelectorAll(".nav-sidebar a");
+
+    if (toggleNavStatus === false) {
+        getSidebarUl.style.visibility = "visible";
+        getSidebar.style.width = "272px";
+        getSidebarTitle.style.opacity = "0.5";
+
+        let arraylength = getSidebarLinks.length; 
+        for (let i = 0; i < arraylength; i++) {
+            getSidebarLinks[i].style.opacity = "1";
+        }
+
+        toggleNavStatus = true;
+    }
+
+    else if (toggleNavStatus === true) {
+        getSidebar.style.width = "50px";
+        getSidebarTitle.style.opacity = "0";
+
+        let arraylength = getSidebarLinks.length; 
+        for (let i = 0; i < arraylength; i++) {
+            getSidebarLinks[i].style.opacity = "0";
+        }
+
+        getSidebarUl.style.visibility = "hidden";
+
+        toggleNavStatus = false;
+    }
+    
+}
+
+const attachEventToChangeColor = function () {
+    const bodyElement = document.body;
+    const Kleur = document.getElementsByClassName("Kleur");
+
+    for (let i = 0;i < Kleur.length; i++){
+        Kleur[i].addEventListener('click', function(){
+            bodyElement.setAttribute("style","background-color: " + Kleur[i].id);
+        });
+    };
+};
+
+attachEventToChangeColor();
